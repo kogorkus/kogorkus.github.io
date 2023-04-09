dcf1b3a6368d8f9d8e8fed830507238076b9ee1e
Vit <kogorkus@seznam.cz>
2023-04-09 06:30:21 +0200

    way more better interactive

---
+++ src/js/script.js
@@ -0,0 +1,35 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector('.menu'),
+    menuItem = document.querySelectorAll('.menu_item'),
+    hamburger = document.querySelector('.burger'),
+    header = document.querySelector('header');
+
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('burger_active');
+        menu.classList.toggle('menu_active');
+        header.classList.toggle('header_active');
+        if (header.classList.contains('header_active')) {
+            header.style.backgroundColor = 'rgba(0, 0, 0, 0.0)';
+        } else
+        {
+            header.style.backgroundColor = 'rgba(39, 52, 86, 1.0)';
+        }
+    });
+
+    menuItem.forEach(item => {
+        item.addEventListener('click', () => {
+            hamburger.classList.toggle('burger_active');
+            menu.classList.toggle('menu_active');
+        })
+    })
+})
+
+function changeCss () {
+    var promoElement = document.querySelector(".promo");
+    var navElement = document.querySelector("header");
+    if (!navElement.classList.contains('header_active')) {
+        this.scrollY > promoElement.offsetHeight ? navElement.style.backgroundColor = 'rgba(39, 52, 86, 1.0)' : navElement.style.backgroundColor = 'rgba(0, 0, 0, 0.3)';
+    }
+  }
+  
+window.addEventListener("scroll", changeCss , false);
